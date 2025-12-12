938303ad0fae0e19c7ce52234ad24a466144d887
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-12-12 20:31:22 -0300

    SPan de erro concluido e mensagem de erro

--- js/modal.js
+++ js/modal.js
@@ -2,10 +2,30 @@ export function modal() {
   const dataBtnModal = document.querySelector("[data-btn-modal]");
   const dataModalContainer = document.querySelector("[data-modal-container]");
   const fileInput = document.querySelector("[data-file]");
+  const dataInputs = document.querySelector("[data-inputs]");
+  let errorFile = false;
+
+  fileInput.addEventListener("change", () => {
+    if (errorFile) {
+      const spanErro = document.querySelector("[data-erro-span]");
+      spanErro.remove();
+    }
+  });
 
   dataBtnModal.addEventListener("click", (e) => {
     if (fileInput.value.length > 0) {
       dataModalContainer.classList.add("remove-modal");
+    } else {
+      messageErro();
     }
   });
+
+  function messageErro() {
+    errorFile = true;
+    const span = document.createElement("span");
+    span.setAttribute("data-erro-span", "");
+    span.classList.add("message-error");
+    span.innerText = "Você deve selecionar um banco de dados";
+    dataInputs.appendChild(span);
+  }
 }
