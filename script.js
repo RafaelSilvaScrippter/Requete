d4ecfe4d4f9a30c443639d4e724971aaabf58c75
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-12-12 18:16:21 -0300

    Modal Concluido

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { editorCode } from "./js/codeEditor.js";
+import { modal } from "./js/modal.js";
 import { tolltip } from "./js/tooltip.js";
 window.addEventListener("DOMContentLoaded", () => {
   editorCode();
   tolltip();
+  modal();
 });
