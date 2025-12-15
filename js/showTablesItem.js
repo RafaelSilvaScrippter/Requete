a2ecae60311b0be380c3c7d8c946972062e977ae
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-12-15 18:09:03 -0300

    Bug corrigido ao mostrar dados da tabela

--- js/showTablesItem.js
+++ js/showTablesItem.js
@@ -1,26 +1,4 @@
 export function showItemTables(dados) {
   const dataUlTables = document.querySelector("[data-ul-schema]");
   const liTables = dataUlTables.querySelectorAll("[data-table-item]");
-
-  liTables.forEach((item) => {
-    item.addEventListener("click", () => {
-      item.innerHTML = "";
-      item.innerHTML = item.dataset.tableItem;
-    });
-  });
-
-  const objectKeys = Object.keys(dados[0]);
-  objectKeys.forEach((item) => {
-    const elemento = document.createElement("li");
-    elemento.innerHTML = `
-    <li>${item}</li>
-    `;
-    elemento.setAttribute("data-li-append", "");
-    elemento.classList.add("li-data-item");
-    liTables.forEach((item) => {
-      item.addEventListener("click", () => {
-        item.append(elemento);
-      });
-    });
-  });
 }
