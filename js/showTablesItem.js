215b02025de239366860b176d796933adc6c7a4a
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-12-15 15:01:47 -0300

    Corrigido o bug do appendchild

--- js/showTablesItem.js
+++ js/showTablesItem.js
@@ -2,8 +2,12 @@ export function showItemTables(dados) {
   const dataUlTables = document.querySelector("[data-ul-schema]");
   const liTables = dataUlTables.querySelectorAll("[data-table-item]");
 
-  const liJs = dataUlTables.querySelectorAll("[data-li-append]");
-  liJs.forEach((li) => li.remove());
+  liTables.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.innerHTML = "";
+      item.innerHTML = item.dataset.tableItem;
+    });
+  });
 
   const objectKeys = Object.keys(dados[0]);
   objectKeys.forEach((item) => {
@@ -12,7 +16,11 @@ export function showItemTables(dados) {
     <li>${item}</li>
     `;
     elemento.setAttribute("data-li-append", "");
-    elemento.classList.add("li-menu-lateral", "li-data-item", "li-tables-item");
-    dataUlTables.appendChild(elemento);
+    elemento.classList.add("li-data-item");
+    liTables.forEach((item) => {
+      item.addEventListener("click", () => {
+        item.append(elemento);
+      });
+    });
   });
 }
