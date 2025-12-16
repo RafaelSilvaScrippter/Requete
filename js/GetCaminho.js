0db84632fbfee2f19656b2af4861a924d38dbf17
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-12-16 14:12:48 -0300

    Modal so fecha se tiver um banco de dados selecionado

--- js/GetCaminho.js
+++ js/GetCaminho.js
@@ -1,7 +1,18 @@
+import { modal } from "./modal.js";
+
 export function getCaminho() {
   const dataFile = document.querySelector("[data-file]");
+  const dataInputs = document.querySelector("[data-erro]");
 
+  let dbSelecionado;
+  modal(dbSelecionado);
   dataFile.addEventListener("click", async (e) => {
-    await window.api.conectarBanco();
+    const conectou = await window.api.conectarBanco();
+    if (!conectou) {
+      dbSelecionado = false;
+      return;
+    }
+    dbSelecionado = true;
+    modal(dbSelecionado, conectou);
   });
 }
